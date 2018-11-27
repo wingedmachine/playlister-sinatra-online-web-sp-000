c5badc9bab641e07ec8db6ffdc51f4b9982f5fa3
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 02:26:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,13 +5,12 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
-  @genres = Genre.all
+    @genres = Genre.all
     erb :'/songs/create'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    binding.pry
     erb :'/songs/show'
   end
 end
