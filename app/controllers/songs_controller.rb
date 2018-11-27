3560a1eedc343595988bfe9d592cfc60d67d3d63
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-27 02:37:18 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,8 @@ class SongsController < ApplicationController
   post '/songs' do
     artist = Artist.find_or_create_by(name: params[:song][:artist_name])
     genres = params[:song][:genre_slugs].map { |slug| Genre.find_by_slug(slug) }
-    Song.create(genres: genres, artist: artist, name: params[:song][:name])
+    song = Song.create(genres: genres, artist: artist, name: params[:song][:name])
+    redirect "/songs/#{song.slug}"
   end
 
   get '/songs/new' do
